1c3eaabd5d4f571c9a60891e509b1f74e4b79f8f
JordiPV <88150345+JorPV@users.noreply.github.com>
2022-06-21 16:58:56 +0200

    1st functionality: calculate the cost for a crop.
    The getCostsForCrop function is working with different inputs.

--- farm.js
+++ farm.js
@@ -1,26 +1,53 @@
-
 // Get yield for plant with no environment factors
-const getYieldForPlant = (plant) => plant.yield; 
+const getYieldForPlant = (plant) => plant.yield;
 
 // Get yield for crop, simple
 const getYieldForCrop = (vegetable) => {
-    const yieldForCrop = vegetable.crop.yield * vegetable.numCrops; 
-    return yieldForCrop;
+  const yieldForCrop = vegetable.crop.yield * vegetable.numCrops;
+  return yieldForCrop;
 };
 
 // Calculate total yield with multiple crops
 // Calculate total yield with 0 amount
-const getTotalYield = (farmCropsYield) => { 
-    let result = 0;
-    farmCropsYield.crops.forEach((crops) => {
-        result += crops.crop.yield * crops.numCrops;
-    })
-    return result
+const getTotalYield = (farmCropsYield) => {
+  let result = 0;
+  farmCropsYield.crops.forEach((crops) => {
+    result += crops.crop.yield * crops.numCrops;
+  });
+  return result;
 };
+
+// Calculate the cost for a crop
+const getCostsForCrop = (plant) => {
+  let costPerCrop = plant.sowingPrice *
+      plant.plantsPerCrop 
+    //   plant.numCrops;
+      return costPerCrop;
+  };
+
+
+// const getCostsForCrop = (plant) => {
+//   let costPerCrop = 0;
+//   plant.plantCrops.forEach((plantCrops) => {
+//    costPerCrop = plantCrops.crop.sowingPrice *
+//       plantCrops.crop.plantsPerCrop *
+//       plantCrops.numCrops;
+//   });
+//   return costPerCrop;
+// };
+
+
+// Calculate the revenue for a crop (without environmental factors)
+const getRevenueForCrop = () => {}
+
+
 
 module.exports = {
-    getYieldForPlant,
-    getYieldForCrop,
-    getTotalYield,
+  getYieldForPlant,
+  getYieldForCrop,
+  getTotalYield,
+  getCostsForCrop,
+  // getRevenueForCrop,
+  // getProfitForCrop,
+  // getTotalProfit
 };
-
--- farm.test.js
+++ farm.test.js
@@ -1,53 +1,92 @@
-const { getYieldForPlant, getYieldForCrop, getTotalYield } = require("./farm");
+const { getYieldForPlant, getYieldForCrop, getTotalYield, getCostsForCrop } = require("./farm");
 
-describe("getYieldForPlant", () => {
-    const corn = {
-        name: "corn",
-        yield: 30,
-    };
+// GIVEN TESTS
+// describe("getYieldForPlant", () => {
+//     const corn = {
+//         name: "corn",
+//         yield: 30,
+//     };
 
-    test("Get yield for plant with no environment factors", () => {
-        expect(getYieldForPlant(corn)).toBe(30);
-    });
-});
+//     test("Get yield for plant with no environment factors", () => {
+//         expect(getYieldForPlant(corn)).toBe(30);
+//     });
+// });
 
-describe("getYieldForCrop", () => {
-    test("Get yield for crop, simple", () => {
+// describe("getYieldForCrop", () => {
+//     test("Get yield for crop, simple", () => {
+//         const corn = {
+//             name: "corn",
+//             yield: 3,
+//         };
+//         const input = {
+//             crop: corn,
+//             numCrops: 10,
+//         };
+//         expect(getYieldForCrop(input)).toBe(30);
+//     });
+// });
+
+// describe("getTotalYield", () => {
+//     test("Calculate total yield with multiple crops", () => {
+//         const corn = {
+//             name: "corn",
+//             yield: 3,
+//         };
+//         const pumpkin = {
+//             name: "pumpkin",
+//             yield: 4,
+//         };
+//         const crops = [
+//             { crop: corn, numCrops: 5 },
+//             { crop: pumpkin, numCrops: 2 },
+//         ];
+//         expect(getTotalYield({ crops })).toBe(23);
+//     });
+
+//     test("Calculate total yield with 0 amount", () => {
+//         const corn = {
+//             name: "corn",
+//             yield: 3,
+//         };
+//         const crops = [{ crop: corn, numCrops: 0 }];
+//         expect(getTotalYield({ crops })).toBe(0);
+//     });
+// });
+
+
+// Funcionality 1: Calculate the cost for a crop
+describe("getCostsForCrop", () => {
+    test("Calculate the cost for a crop", () => {
         const corn = {
             name: "corn",
-            yield: 3,
-        };
-        const input = {
-            crop: corn,
+            sowingPrice: 1,
+            plantsPerCrop: 50,
             numCrops: 10,
-        };
-        expect(getYieldForCrop(input)).toBe(30);
-    });
-});
+        }
 
-describe("getTotalYield", () => {
-    test("Calculate total yield with multiple crops", () => {
-        const corn = {
-            name: "corn",
-            yield: 3,
-        };
         const pumpkin = {
-            name: "pumpkin",
-            yield: 4,
+            name: "pumpkin", 
+            sowingPrice: 1,
+            plantsPerCrop: 25,
+            numCrops:20,
         };
-        const crops = [
-            { crop: corn, numCrops: 5 },
-            { crop: pumpkin, numCrops: 2 },
-        ];
-        expect(getTotalYield({ crops })).toBe(23);
-    });
 
-    test("Calculate total yield with 0 amount", () => {
-        const corn = {
-            name: "corn",
-            yield: 3,
-        };
-        const crops = [{ crop: corn, numCrops: 0 }];
-        expect(getTotalYield({ crops })).toBe(0);
+        const tomatoes = {
+            name: "tomatoes", 
+            sowingPrice: 1,
+            plantsPerCrop: 40,
+            numCrops: 25,
+        }
+
+        expect(getCostsForCrop(tomatoes)).toBe(40);
+    
     });
 });
+
+
+// Functionality 2: calculate the revenue for a crop (without environmental factors)
+describe("getRevenueForCrop", () => {
+    test("calculate the revenue for a crop (without environmental factors)", () => {
+        
+    })
+})
